070d7a23ea285614848d62df3b6b07c29a505d0d
yasuto <fujiya0101@gmail.com>
2022-02-28 21:07:37 +0900

    マッチ戻す

--- src/match.js
+++ src/match.js
@@ -2,54 +2,38 @@ import axios from "axios";
 var interval_id = null;
 var flg = false;
 var data;
-// interval_id = setInterval(async () => {
-//   if (!flg) {
-//     try {
-//       let surl = '/matchRequest';
-//       let request;
-//       request = await axios.get(surl);
-//       data = request.data;
-//       if (request.data.judg == true) {
-//         // stopInterval(data);
-//         flg = true;
-//       }
-//     } catch (er) {
-//       console.log(request)
-//       console.log(er);
-//     }
-//   } else {
-//     try {
-//       let surl = '/matchconf?id=' + data.roomid;
-//       let request = await axios.get(surl);
-//       let pdata = request.data;
-//       if (pdata.judg == true) {
-//         console.log(pdata.judg);
-//         stopInterval(data);
-//       }
-//     } catch (er) {
-//       console.log(request)
-//       console.log(er)
-//     }
-//   }
-// }, 5000);
-
-// function stopInterval(urldata) {
-//   clearInterval(interval_id);
-//   location.href = `/game?player_id=${urldata.playerid}&roomid=${urldata.roomid}`;
-// }
-async function test() {
-  try {
-    let surl = '/test1';
-    let request;
-    request = await axios.post(surl);
-    // data = request.data;
-    // if (request.data.judg == true) {
-    //   // stopInterval(data);
-    //   flg = true;
-    // }
-    console.log(request);
-  } catch (er) {
-    console.log(er);
+interval_id = setInterval(async () => {
+  if (!flg) {
+    try {
+      let surl = '/matchRequest';
+      let request;
+      request = await axios.get(surl);
+      data = request.data;
+      if (request.data.judg == true) {
+        // stopInterval(data);
+        flg = true;
+      }
+    } catch (er) {
+      console.log(request)
+      console.log(er);
+    }
+  } else {
+    try {
+      let surl = '/matchconf?id=' + data.roomid;
+      let request = await axios.get(surl);
+      let pdata = request.data;
+      if (pdata.judg == true) {
+        console.log(pdata.judg);
+        stopInterval(data);
+      }
+    } catch (er) {
+      console.log(request)
+      console.log(er)
+    }
   }
+}, 5000);
+
+function stopInterval(urldata) {
+  clearInterval(interval_id);
+  location.href = `/game?player_id=${urldata.playerid}&roomid=${urldata.roomid}`;
 }
-test();
